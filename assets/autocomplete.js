903f9b1b9b538011cf936f455282be4a1269d098
shadd0w <rene.gmitter@wame.sk>
2016-09-16 16:06:24 +0200

    UPDATE Admin group actions

---
+++ assets/autocomplete.js
@@ -0,0 +1,74 @@
+(function($) {
+    $.fn.wameAutocomplete = function(options) {
+        var defaultOptions = {
+            source: null,
+            columns: null,
+            select: null,
+            minLength: 3
+        };
+      
+        var options = $.extend(defaultOptions, options);
+        
+        return this.each(function() {
+            var $obj = $(this);
+
+            $obj.autocomplete({
+                source: function( request, response ) {
+                    var phrase = request.term;
+                    var source = options.source || $obj.data('source');
+                    var dataOptions = $obj.data('options');
+                    var columns = options.columns || dataOptions.columns;
+                    var select = options.select || dataOptions.select;
+
+                    $.ajax({
+                        url: source,
+                        type: 'get',
+                        dataType: 'json',
+                        data: { 
+                            columns : columns,
+                            phrase : phrase,
+                            select : select
+                        },
+                        success: function( res ) {
+                            if (res.length === 0) {
+                                return {
+                                    label: 'No results found',
+                                    value: ''
+                                };
+                            } else {
+                                response($.map(res, function(item) {
+                                    return {
+                                        label: item.title,
+                                        value: item.id
+                                    };
+                                }));
+                            }
+                        }
+                    });
+                },
+                minLength: options.minLength,
+                select: function( event, ui ) {
+                    var value = ui.item.value;
+                    var label = ui.item.label;
+
+                    $obj.attr('value', value).hide();
+                    $obj.before('<div class="'+$obj.attr('class')+' autocomplete-value">' + label + '<a class="autocomplete-remove btn btn-xs btn-link pull-right" href="#" data-control="' + $obj.attr('id') + '"><span class="fa fa-times-circle"></span></a></div>');
+                },
+                open: function() {
+                    $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+                },
+                close: function() {
+                    $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+                }
+            });
+            
+            $(document).on('click', '.autocomplete-remove', function(e) {
+                e.preventDefault();
+
+                // TODO: oprav lukas!
+                $(this).closest('.autocomplete-value').remove();
+                $obj.val('').show();
+            });
+        });
+    };
+})(jQuery);
